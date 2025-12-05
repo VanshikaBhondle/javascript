5e72c00dbb930bc59dc47c316b5e5eb84a886a21
VANSHIKA BHONDLE <bhondlevanshika@gmail.com>
2025-12-05 13:29:21 +0000

    comparison and datatype_summary

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log("2">1);
+// console.log("02">1);
+// problem   because comparison is between two different  data types
+
+console.log(null >0);
+console.log(null == 0);
+console.log(null>=0);// null treat itself as 0 
+
+
+console.log(undefined ==0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// comparing null and undefined  is not good practice because  we donot know ki ky typeconversion hone wala hai its a bad practice
+// == type conversion kr deta hai vheck krte time 
+// === strict conversion  isme data type bhi check hota hai 
+
+console.log("2"===2);
+
+
+
+
+
+ 
---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,50 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+//Primitive: These are call-by-value. The types of primitive data types include String, Number, Boolean, Null, Undefined, Symbol, and BigInt. Note that Symbols uniquely identify variables, even if two variables have the same data and data type. Primitive variables datatype is same but null datatype is object 
+// Non-primitive: These are call-by-reference. The types of non-primitive data types are Array, Object, and Function. The data type for these is 'object,' but for functions, they return a data type of 'function,' though they also have an object data type.
+
+// Return type of variables in JavaScript
+//  Primitive Datatypes
+//    1) Number => number
+//    2) String  => string
+//     3)   Boolean  => boolean
+//     4) null  => object
+//     5) undefined  =>  undefined
+//     6) Symbol  =>  symbol
+//     7) BigInt  =>  bigint
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
