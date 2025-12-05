dbaadc091403aec84ea22f2ae086beb9c70c8fcc
VANSHIKA BHONDLE <bhondlevanshika@gmail.com>
2025-12-05 12:53:31 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,26 +1,79 @@
 let score = null
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber =Number(score)
-console .log(typeof valueInNumber );
-console.log (valueInNumber);
+// console .log(typeof valueInNumber );
+// console.log (valueInNumber);
 
 // "33" =>33
 // "33abc" =>NaN
 // true =>1; false=>0
 let isLoggedIn =1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(isLoggedIn);
+// console.log(booleanIsLoggedIn);
 // 1 =>true; 0=>false
 //"" => false
 // "hitesh" =>true
 
 let someNumber= 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+//*************************OPERATION****************************
+
+let value= 3
+let negvalue= -value
+//console.log(negvalue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2%3);
+
+// console.log(2/3);
+let str1 ="hello";
+let str2 = "shiv";
+let str3 = str1+ str2
+// console.log(str3);
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log((3+4)*5%3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+num1=num2=num3= 2+2
+
+let gamecounter=100
+gamecounter++;
+console.log(gamecounter);
+
+
+// EXAMPLE OF PREFIX TO POSTFIX
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+
+
 
 
