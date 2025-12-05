575fcccff769f29dca213a2c14eb849ff2fc10b7
VANSHIKA BHONDLE <bhondlevanshika@gmail.com>
2025-12-05 10:27:35 +0000

    variable datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountID=14335
+let accountEmail="vanshika@gmail"
+var accountPassword = "12345"
+accountCity="nagpur"
+let accountState;
+
+// accountID =2//not allowed//
+accountEmail="alik@.v.com"
+accountPassword="5678"
+accountCity="bengluru"
+
+console.log(accountID);
+// prefer not to use "var"
+//because issue in block scope and functional scope //
+
+console.log([accountEmail,accountPassword,accountCity]);
+console.table([accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all  JS code as newer version
+// alert(3+ 3) // we are using nodejs,not browser
+
+console.log(3
+    +
+    3)// code readability  must be
+console.log(3+3)    
+
+console.log("hitesh")
+let name= vansh
+let age =20
+let isLoggedIn= true
+let state = null
+
+// number => 2 to power 53
+// big int
+// string " "
+// boolean => true/false
+// null => standalone value /empty
+// undefined =>  abhi value assign nhi hui hai
+// symbol => unique
+
+// object 
+
+console.log(typeof "vansh");
+console.log(typeof "age");
+console.log(typeof "null");
+console.log(typeof undefined);
+
+
